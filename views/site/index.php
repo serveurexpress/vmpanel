a93a20474e0d651e212bbfe264c4e4d1e4c0b745
Thomas <thomas@alphaweb.fr>
2015-08-18 17:18:31 +0200

    UPD index

--- views/site/index.php
+++ views/site/index.php
@@ -18,7 +18,7 @@ $this->title = 'VMpanel';
                     </div>
                     <div class="panel-footer">
                        <a href="/index.php?vm='.$vm.'&action=start" class="btn btn-default" title="Démarrer">'.Icon::show('play').'</a>'
-                    . '<a href="/index.php?vm='.$vm.'&action=pause" class="btn btn-default" title="Pause">'.Icon::show('pause').'</a>'
+//                    . '<a href="/index.php?vm='.$vm.'&action=pause" class="btn btn-default" title="Pause">'.Icon::show('pause').'</a>'
                     . '<a href="/index.php?vm='.$vm.'&action=stop" class="btn btn-default" title="Arrêter">'.Icon::show('stop').'</a>'
                     . '<a href="/index.php?vm='.$vm.'&action=restart" class="btn btn-default" title="Relancer">'.Icon::show('refresh').'</a>'
                     . '<a href="/index.php?vm='.$vm.'&action=fsck" class="btn btn-default" title="Fsck">'.Icon::show('search').'</a>
