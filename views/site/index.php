d156cf0f0830000f265d424d56fef78bf411e18e
Thomas <thomas@alphaweb.fr>
2015-08-18 15:29:56 +0200

    UPD index

--- views/site/index.php
+++ views/site/index.php
@@ -6,8 +6,18 @@ $this->title = 'VMpanel';
 <div class="site-index">
     <div class="body-content">
         <?php
+        $result = "";
         foreach ($vmlist as &$vm) {
-            echo $vm;
+            $result = '
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">'.$vm.'</h3>
+                    </div>
+                    <div class="panel-body">
+                        Panel content
+                    </div>
+                </div>';
+            echo $result;
         }
         ?>
     </div>
