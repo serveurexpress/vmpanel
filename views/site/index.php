e7ec1cbd44401a7dc35f875d44ffd76d37bd9810
Thomas <thomas@alphaweb.fr>
2015-08-20 14:30:31 +0200

    UPD log

--- views/site/index.php
+++ views/site/index.php
@@ -22,17 +22,19 @@ $this->title = 'VMpanel';
                 $status = false;
                 $stopStatus = 'disabled';
             }
-            $log = file_get_contents(Yii::$app->params['logDir'] . '/' . $vm . '.log');
+            if (file_exists(Yii::$app->params['logDir'] . '/' . $vm . '.log')) {
+                $log = file_get_contents(Yii::$app->params['logDir'] . '/' . $vm . '.log');
+            }
             $buttonMenu = '<a href="/index.php?vm=' . $vm . '&action=start" class="btn btn-default" title="Démarrer" ' . $startStatus . '>' . Icon::show('play') . '</a>'
                     . '<a href="/index.php?vm=' . $vm . '&action=stop" class="btn btn-default" title="Arrêter" ' . $stopStatus . '>' . Icon::show('stop') . '</a>'
                     . '<a href="/index.php?vm=' . $vm . '&action=restart" class="btn btn-default" title="Relancer" ' . $stopStatus . '>' . Icon::show('refresh') . '</a>'
                     . '<a href="/index.php?vm=' . $vm . '&action=fsck" class="btn btn-default" title="Fsck" ' . $startStatus . '>' . Icon::show('search') . '</a>';
             $actionMenu = '<div class="row"><div class="col-md-12">' . $buttonMenu . '</div></div><br />';
-            $actionMenu .= '<div class="row"><div class="col-md-12">'.Html::textarea($vm . 'ActionResult', "Dernières actions : \n".$log, ['id' => $vm . 'ActionResult', 'class' => 'form-control', 'rows' => '6']).'</div></div>';
+            $actionMenu .= '<div class="row"><div class="col-md-12">' . Html::textarea($vm . 'ActionResult', "Dernières actions : \n" . $log, ['id' => $vm . 'ActionResult', 'class' => 'form-control', 'rows' => '6']) . '</div></div>';
             $list = glob(Yii::$app->params['actionDir'] . $vm . '-' . Yii::$app->params['hosterName'] . '-*');
             if (count($list) > 0) {
                 $actionMenu = '<div class="row hidden"><div class="col-md-12">' . $buttonMenu . '</div></div><br />';
-                $actionMenu .= '<div class="row"><div class="col-md-12">'.Html::textarea($vm . 'ActionResult', 'Une action est en cours :', ['id' => $vm . 'ActionResult', 'class' => 'form-control', 'rows' => '6']).'</div></div>';
+                $actionMenu .= '<div class="row"><div class="col-md-12">' . Html::textarea($vm . 'ActionResult', 'Une action est en cours :', ['id' => $vm . 'ActionResult', 'class' => 'form-control', 'rows' => '6']) . '</div></div>';
             }
             $result = '
                 <div class="panel panel-default">
