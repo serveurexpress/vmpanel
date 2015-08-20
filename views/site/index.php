512c52dbe35d37992af74fb0bba11a88994a5e10
Thomas <thomas@alphaweb.fr>
2015-08-20 15:12:34 +0200

    ADD img if

--- views/site/index.php
+++ views/site/index.php
@@ -40,7 +40,10 @@ $this->title = 'VMpanel';
                 $actionMenu = '<div class="row hidden"><div class="col-md-12">' . $buttonMenu . '</div></div><br />';
                 $actionMenu .= '<div class="row"><div class="col-md-12"><label class="control-label">Une action est en cours</label>' . Html::textarea($vm . 'ActionResult', '', ['id' => $vm . 'ActionResult', 'class' => 'form-control', 'rows' => '6']) . '</div></div>';
             }
-            $imgIf = 'http://'.Yii::$app->params['hosterName'] . '.x1.fr' . Yii::$app->params['rrdDir'] . 'tap' . substr($vm, 1) . '-daily.png';
+            $imgEthDaily = 'http://'.Yii::$app->params['hosterName'] . '.x1.fr' . Yii::$app->params['rrdDir'] . 'tap' . substr($vm, 1) . '-daily.png';
+            $imgEthWeekly = 'http://'.Yii::$app->params['hosterName'] . '.x1.fr' . Yii::$app->params['rrdDir'] . 'tap' . substr($vm, 1) . '-weekly.png';
+            $imgEthMonthly = 'http://'.Yii::$app->params['hosterName'] . '.x1.fr' . Yii::$app->params['rrdDir'] . 'tap' . substr($vm, 1) . '-monthly.png';
+            $imgEthYearly = 'http://'.Yii::$app->params['hosterName'] . '.x1.fr' . Yii::$app->params['rrdDir'] . 'tap' . substr($vm, 1) . '-yearly.png';
             $result = '
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -56,7 +59,14 @@ $this->title = 'VMpanel';
                     ]) . '</h3>
                     </div>
                     <div class="panel-body">
-                        <img class="img-responsive" src="' . $imgIf . '">
+                    <div class="row">
+                        <div class="col-md-6 col-lg-6"><img class="img-responsive" src="' . $imgEthDaily . '"></div>
+                        <div class="col-md-6 col-lg-6"><img class="img-responsive" src="' . $imgEthWeekly . '"></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 col-lg-6"><img class="img-responsive" src="' . $imgEthMonthly . '"></div>
+                        <div class="col-md-6 col-lg-6"><img class="img-responsive" src="' . $imgEthYearly . '"></div>
+                    </div>
                     </div>
                     <div class="panel-footer">
                        ' . $actionMenu . '
