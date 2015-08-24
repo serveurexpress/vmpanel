0ab7fe87fb7990ab86b4e42a0d31ed4dd426e17f
Thomas <thomas@alphaweb.fr>
2015-08-24 15:20:09 +0200

    UPD pjax

--- views/site/index.php
+++ views/site/index.php
@@ -35,10 +35,10 @@ $this->title = 'VMpanel';
             $err = '';
             $status = trim(shell_exec('sudo ' . Yii::$app->params['scriptDir'] . Yii::$app->params['scriptStatus'] . ' ' . $vm));
             if ($status == '1') {
-                $status = true;
+                $status = '<i class="fa fa-power-off poweron"></i>';
                 $startStatus = 'disabled';
             } else {
-                $status = false;
+                $status = '<i class="fa fa-power-off poweroff"></i>';
                 $stopStatus = 'disabled';
             }
             if (file_exists(Yii::$app->params['logDir'] . '/' . $vm . '.log')) {
@@ -61,8 +61,7 @@ $this->title = 'VMpanel';
                 $buttonsMenu = '<div class="row hidden">' . $buttons . '</div>' .
                         ' <div class="progress">
                             <div id="progress-' . $vm . '" class="progress-bar progress-bar-striped active" role="progressbar"
-                            aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:10%">
-                              10%
+                            aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%">
                             </div>
                           </div>';
                 $actionMenu = '<div id="logs' . $vm . '" class="panel-footer"><div class="row"><div class="col-md-12"><label class="control-label">Une action est en cours</label>' . Html::textarea($vm . 'ActionResult', $log, ['id' => $vm . 'ActionResult', 'class' => 'form-control', 'rows' => '6']) . '</div></div></div>';
@@ -105,20 +104,7 @@ $this->title = 'VMpanel';
                             </button>
                             </div>
                             <div class="col-md-4 text-center">' . $buttonsMenu . '</div>
-                            <div class="col-md-4 text-right">' . SwitchInput::widget([
-                        'name' => 'status_1',
-                        'value' => $status,
-                        'disabled' => true,
-                        'pluginOptions' => [
-                            'size' => 'small',
-                            'onColor' => 'success',
-                            'offColor' => 'danger',
-                        ],
-                        'containerOptions' => [
-                            'class' => 'noForm',
-                        ],
-                    ]) . '
-                            </div>
+                            <div class="col-md-4 text-right">' . $status . ' </div>
                             </div>
                     </div>
                     <div id="graph' . $vm . '" class="panel-body collapse">
