1d347ccf644f147a429a8263a76deb63779e10d4
Thomas <thomas@alphaweb.fr>
2015-08-24 14:45:14 +0200

    UPD pjax

--- views/site/index.php
+++ views/site/index.php
@@ -68,11 +68,11 @@ $this->title = 'VMpanel';
                 $actionMenu = '<div id="logs' . $vm . '" class="panel-footer"><div class="row"><div class="col-md-12"><label class="control-label">Une action est en cours</label>' . Html::textarea($vm . 'ActionResult', $log, ['id' => $vm . 'ActionResult', 'class' => 'form-control', 'rows' => '6']) . '</div></div></div>';
                 $this->registerJs('$(document).ready(function(){
                     if ($("#progress-' . $vm . '").length > 0) {
-                        timeoutID' . $vm . ' = setTimeout(function(){
+                        intervalID' . $vm . ' = setInterval(function(){
                             $.pjax.reload({container:"#pjax-' . $vm . '"});
                         },2000);
                     } else {
-                        //clearTimeout(timeoutID' . $vm . ');
+                        clearInterval(intervalID' . $vm . ');
                     }
               });', View::POS_END);
             }
