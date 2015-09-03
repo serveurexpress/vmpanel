dd083824b626a3c3fe26e290062edd7c7444bcbd
thomas <thomas@ns12.alphaweb.fr>
2015-09-03 17:48:05 +0200

    ADD exemple vmlist for admin

--- views/user/admin/_user.php
+++ views/user/admin/_user.php
@@ -11,3 +11,4 @@
 <?= $form->field($user, 'email')->textInput(['maxlength' => 255]) ?>
 <?= $form->field($user, 'password')->passwordInput() ?>
 <?= $form->field($user, 'vmlist')->textInput(['maxlength' => 255]) ?>
+VMLIST : liste de nom de machine séparé par des virgules sans espace (EX : t134,t135,t136)
