8ad9413a07a1f553b343492c2b9ea1549c2ddf8a
thomas <thomas@ns12.alphaweb.fr>
2015-09-03 17:33:45 +0200

    TEST sqlite

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-//    'dsn' => 'sqlite:/etc/vmpanel/vmpanel.db',
-    'dsn' => 'mysql:host=localhost;dbname=vmpanel',
-    'username' => 'vmpanel',
-    'password' => 'cvSvBGUvKRdECtfN',
+    'dsn' => 'sqlite:/etc/vmpanel/vmpanel.db',
+//    'dsn' => 'mysql:host=localhost;dbname=vmpanel',
+//    'username' => 'vmpanel',
+//    'password' => 'cvSvBGUvKRdECtfN',
     'charset' => 'utf8',
 ];
