93654d1bf5cb3bbd299ff47b38acb083a7cb2932
thomas <thomas@ns12.alphaweb.fr>
2015-09-04 12:07:37 +0200

    UPD dynamique hostname

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -28,7 +28,7 @@ AppAsset::register($this);
         <div class="wrap">
             <?php
             NavBar::begin([
-                'brandLabel' => 'VMpanel',
+                'brandLabel' => 'VMpanel '.  ucfirst(gethostname()),
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
